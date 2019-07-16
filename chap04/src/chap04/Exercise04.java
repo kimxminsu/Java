0f53cbbbff9e9b07e4c1f881e548e4ff80bdef8e
Minsuuu <sd15615@gmail.com>
2019-07-16 20:59:39 +0900

    190716 Exercise

--- chap04/src/chap04/Exercise04.java
+++ chap04/src/chap04/Exercise04.java
@@ -6,9 +6,10 @@ public class Exercise04 {
 		int num1=0;
 		int num2=0;
 		while(num1+num2!=5) {
-			System.out.println(num1 + "," + num2);
+			System.out.println("(" + num1 + ", " + num2 + ")");
 			num1 = (int)(Math.random()*6) +1;
 			num2 = (int)(Math.random()*6) +1;
 		}
+		System.out.println("(" + num1 + ", " + num2 + ")");
 	}
 }
