307fa864684d31d0cbde63b57ada42c83ba80f75
Minsuuu <sd15615@gmail.com>
2019-07-31 16:40:39 +0900

    190731

---
+++ chap04/src/chap04/Exercise07.java
@@ -0,0 +1,15 @@
+package chap04;
+
+import java.util.Scanner;
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+		boolean run = true;
+		
+		int balance = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+	}
+
+}
