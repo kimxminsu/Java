6f821d2b3f4025076ee83ede461255824f82de19
Minsuuu <sd15615@gmail.com>
2019-12-06 18:18:46 +0900

    191206 Servlet

---
+++ Test/src/Hello.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet{
+	@Override //doGet
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out=resp.getWriter();
+		out.println("Hello, Servlet");
+	}
+}
