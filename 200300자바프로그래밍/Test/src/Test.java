3f15f9f9b1cf57c37ce683286ac081b11a55d680
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 02:12:47 +0900

    변수,자료형

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,15 +1,13 @@
 public class Test {
 	public static void main(String[] args) {
-		String name="김철수";
-		int age=21;
-		double weight=65.7;
-		char gender='남';
-		boolean dopingTestPassed=true;
+		int box1Weight = 50;
+		int box2Weight = 300;
 		
-		System.out.println(name); // 선수 이름
-		System.out.println(age); // 선수 나이
-		System.out.println(weight); // 선수 체중
-		System.out.println(gender); // 선수 성별
-		System.out.println(dopingTestPassed); // 선수 도핑 테스트 결과 (불리언)
+		int temp = box1Weight;
+		box1Weight = box2Weight;
+		box2Weight = temp;
+		
+		System.out.println(box1Weight);
+		System.out.println(box2Weight);
 	}
 }
