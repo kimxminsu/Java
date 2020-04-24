95d95b8bef69326ca29b9f1c412c78cd5fab0067
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 22:49:10 +0900

    조건문_if

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,15 +1,8 @@
-import javax.swing.JOptionPane;
-
 public class Test {
 	public static void main(String[] args) {
-		String num1 = JOptionPane.showInputDialog("사칙연산 계산을 위한 첫번째 정수를 입력하세요");
-		String num2 = JOptionPane.showInputDialog("사칙연산 계산을 위한 두번째 정수를 입력하세요");
-		double n1 = Integer.parseInt(num1);
-		double n2 = Integer.parseInt(num2);
-		String message = n1 + " + " + n2 + " = " + (n1 + n2) + "\n" 
-				+ n1 + " - " + n2 + " = " + (n1 - n2) + "\n"
-				+ n1 + " X " + n2 + " = " + (n1 * n2) + "\n"
-				+ n1 + " / " + n2 + " = " + (n1 / n2) + "\n";
-		JOptionPane.showMessageDialog(null, message);
+		int score = -8;
+		if (score < 0)
+			score = score * (-1);
+		System.out.println(score);
 	}
 }
