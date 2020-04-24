1b94f15deb8b4e4a882635085cdbbdd3adefdd35
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 22:49:51 +0900

    조건문_삼항연산자

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,8 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 		int score = -8;
-		if (score < 0)
-			score = score * (-1);
+		score = (score < 0) ? score * (-1) : score;
 		System.out.println(score);
 	}
 }
