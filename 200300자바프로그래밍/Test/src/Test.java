f8c9069e0bc0118fe99bbcf02fd8154c197c727f
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 13:24:29 +0900

    변수,자료형

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,11 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class Test {
 	public static void main(String[] args) {
-		double x = 40;
-		double y = 8;
-
-		System.out.println(x + y);
-		System.out.println(x - y);
-		System.out.println(x * y);
-		System.out.println(x / y);
+		String fahrenheitStr = JOptionPane.showInputDialog("화씨 온도 값을 입력하세요");
+		double fahrenheit = Integer.parseInt(fahrenheitStr);
+		double celsius = (fahrenheit-32)*5/9;
+		String message = "화씨 " +fahrenheit+"도는 섭씨 "+celsius+"도입니다.";
+		JOptionPane.showMessageDialog(null, message);
 	}
 }
