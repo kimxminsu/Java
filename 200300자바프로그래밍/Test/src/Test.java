002c6d44d6f28621d3e526627df4af963cd23b51
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 13:25:08 +0900

    Convert miles to kilometers

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -2,10 +2,10 @@ import javax.swing.JOptionPane;
 
 public class Test {
 	public static void main(String[] args) {
-		String fahrenheitStr = JOptionPane.showInputDialog("화씨 온도 값을 입력하세요");
-		double fahrenheit = Integer.parseInt(fahrenheitStr);
-		double celsius = (fahrenheit-32)*5/9;
-		String message = "화씨 " +fahrenheit+"도는 섭씨 "+celsius+"도입니다.";
+		String mileStr = JOptionPane.showInputDialog("마일(mile)값을 입력하세요");
+		int mile = Integer.parseInt(mileStr);
+		double km = mile * 1.609;
+		String message = mile + "마일(mile)은 " + km + " 킬로미터(km)입니다.";
 		JOptionPane.showMessageDialog(null, message);
 	}
 }
