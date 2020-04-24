edc40545c93ad21e001be6cd705bce7ccfe0550f
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-24 14:46:19 +0900

    calculation

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -2,10 +2,14 @@ import javax.swing.JOptionPane;
 
 public class Test {
 	public static void main(String[] args) {
-		String fahrenheitStr = JOptionPane.showInputDialog("화씨 온도 값을 입력하세요");
-		double fahrenheit = Integer.parseInt(fahrenheitStr);
-		double celsius = (fahrenheit - 32) * 5 / 9;
-		String message = "화씨 " + fahrenheit + "도는 섭씨 " + celsius + "도입니다.";
+		String num1 = JOptionPane.showInputDialog("사칙연산 계산을 위한 첫번째 정수를 입력하세요");
+		String num2 = JOptionPane.showInputDialog("사칙연산 계산을 위한 두번째 정수를 입력하세요");
+		double n1 = Integer.parseInt(num1);
+		double n2 = Integer.parseInt(num2);
+		String message = n1 + " + " + n2 + " = " + (n1 + n2) + "\n" 
+				+ n1 + " - " + n2 + " = " + (n1 - n2) + "\n"
+				+ n1 + " X " + n2 + " = " + (n1 * n2) + "\n"
+				+ n1 + " / " + n2 + " = " + (n1 / n2) + "\n";
 		JOptionPane.showMessageDialog(null, message);
 	}
 }
