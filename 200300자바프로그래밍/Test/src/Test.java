a42148a8550521078c5400079e6d952c76beb5fe
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-25 20:52:01 +0900

    조건문_점수 정상여부

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,13 +1,9 @@
 public class Test {
 	public static void main(String[] args) {
-		double fee = 1000;
-		int age = 65;
-		if (age <= 6)
-			fee *= 0.5;
-		if (age >= 65)
-			fee *= 0.7;
-		if (age >= 7 && age < 20)
-			fee *= 0.8;
-		System.out.println(fee);
+		int score = -1;
+		if (score >= 0 && score <= 100)
+			System.out.println("정상");
+		else
+			System.out.println("비정상");
 	}
 }
