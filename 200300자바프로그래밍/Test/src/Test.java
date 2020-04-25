3451cd058974161e719f297bbb26bbdbf7026400
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-25 21:53:15 +0900

    조건식_세개 중 가장큰수 출력

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,10 +1,16 @@
 public class Test {
 	public static void main(String[] args) {
-		int x = 23, y = 89;
+		int x = 237, y = 89, z = -6;
 		if (x > y) {
-			System.out.println(x);
+			if (x > z)
+				System.out.println(x);
+			else
+				System.out.println(z);
 		} else {
-			System.out.println(y);
+			if (y > z)
+				System.out.println(y);
+			else
+				System.out.println(z);
 		}
 	}
 }
