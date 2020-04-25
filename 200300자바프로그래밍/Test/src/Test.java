7929c45f4c4e6d008946559d8cb279d07c4850c9
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-25 21:39:30 +0900

    조건문_점수 정상여부_삼항연산자

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,9 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
 		int score = -1;
-		if (score >= 0 && score <= 100)
-			System.out.println("정상");
-		else
-			System.out.println("비정상");
+		String prt;
+		prt = (score >= 0 && score <= 100) ? "정상" : "비정상";
+		System.out.println(prt);
 	}
 }
