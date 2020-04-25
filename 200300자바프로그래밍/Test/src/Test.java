3de25d20a466e2e4620b0b99cb319d7f08c2e0d6
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-25 20:33:58 +0900

    조건문_입장료

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,7 +1,11 @@
 public class Test {
 	public static void main(String[] args) {
-		int score = -8;
-		score = (score < 0) ? score * (-1) : score;
-		System.out.println(score);
+		double fee = 1000;
+		int age = 65;
+		if (age <= 6)
+			fee *= 0.5;
+		if (age >= 65)
+			fee *= 0.7;
+		System.out.println(fee);
 	}
 }
