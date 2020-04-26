7adcdc557d4006d5e3ca73b004528709763b8df3
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-27 02:42:08 +0900

    반복문_1부터100까지 홀수합

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,25 +1,11 @@
-import javax.swing.JOptionPane;
-
 public class Test {
 	public static void main(String[] args) {
-		String scoreStr = JOptionPane.showInputDialog("성적(0-100)을 입력하세요");
-		int score = Integer.parseInt(scoreStr);
-		char grade = ' ';
-		if (score >= 90) {
-			grade = 'A';
+		int i = 0, sum = 0;
+		while (i <= 100) {
+			if (i % 2 == 1)
+				sum += i;
+			i++;
 		}
-		if (score >= 80 && score < 90) {
-			grade = 'B';
-		}
-		if (score >= 70 && score < 80) {
-			grade = 'C';
-		}
-		if (score >= 60 && score < 70) {
-			grade = 'D';
-		}
-		if (score < 70)
-			grade = 'F';
-		String message = "성적 " + score + " 의 학점은 " + grade + "입니다.";
-		JOptionPane.showMessageDialog(null, message);
+		System.out.println(sum);
 	}
 }
