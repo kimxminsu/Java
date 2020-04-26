38799cbe3e87e7995a2f53c390d6b2ae2a283633
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-27 02:44:42 +0900

    반복문_1부터10까지 곱

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,10 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
-		int i = 0, sum = 0;
-		while (i <= 100) {
-			if (i % 2 == 1)
-				sum += i;
-			i++;
+		int sum = 1;
+		for (int i = 1; i <= 10; i++) {
+			sum *= i;
 		}
 		System.out.println(sum);
 	}
