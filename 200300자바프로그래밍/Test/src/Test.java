98f7514d39c80bb1c333152a4ab0858fc3d6d171
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-27 02:06:34 +0900

    조건문_성적_JOptionPane

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,19 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class Test {
 	public static void main(String[] args) {
-		int score = 95;
+		String scoreStr = JOptionPane.showInputDialog("성적(0-100)을 입력하세요");
+		int score = Integer.parseInt(scoreStr);
+		char grade = ' ';
 		if (score >= 90) {
-			System.out.println('A');
+			grade = 'A';
 		}
 		if (score >= 80 && score < 90) {
-			System.out.println('B');
+			grade = 'B';
 		}
 		if (score >= 70 && score < 80) {
-			System.out.println('C');
+			grade = 'C';
 		}
 		if (score >= 60 && score < 70) {
-			System.out.println('A');
+			grade = 'D';
 		}
 		if (score < 70)
-			System.out.println('F');
+			grade = 'F';
+		String message = "성적 " + score + " 의 학점은 " + grade + "입니다.";
+		JOptionPane.showMessageDialog(null, message);
 	}
 }
