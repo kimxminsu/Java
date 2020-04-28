cad481e8706749ea2f8d6897dbafaa02ba1abd73
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-28 17:40:59 +0900

    반복문_성적_during click cancle

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,9 +1,29 @@
+import javax.swing.JOptionPane;
+
 public class Test {
 	public static void main(String[] args) {
-		int sum = 1;
-		for (int i = 1; i <= 10; i++) {
-			sum *= i;
-		}
-		System.out.println(sum);
+		while(true) {
+			String scoreStr = JOptionPane.showInputDialog("성적(0-100)을 입력하세요");
+			double score = Integer.parseInt(scoreStr);
+			if(scoreStr==null) break;
+			char grade = ' ';
+			if (score >= 90) {
+				grade = 'A';
+			}
+			if (score >= 80 && score < 90) {
+				grade = 'B';
+			}
+			if (score >= 70 && score < 80) {
+				grade = 'C';
+			}
+			if (score >= 60 && score < 70) {
+				grade = 'D';
+			}
+			if (score < 70)
+				grade = 'F';
+			String message = "성적 " + score + " 의 학점은 " + grade + "입니다.";
+			JOptionPane.showMessageDialog(null, message);
+			}
+		
 	}
 }
