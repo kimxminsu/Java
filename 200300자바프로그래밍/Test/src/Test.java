3bf1aaac9a640ce3c8a714508b48a368115d5d85
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-30 00:37:18 +0900

    배열_double

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,21 +1,17 @@
 /*
-'K', 'o', 'r', 'e', 'a'를 배열 country에 저장하는 코드를 작성하시오
+94.5를 배열 score에 저장하는 코드를 작성하시오
 3가지 서로 다른 방법으로 작성해 보시오
 */
 public class Test {
 	public static void main(String[] args) {
-		char country[];
-		country=new char[5];
-		country[0]='K';
-		country[1]='o';
-		country[2]='r';
-		country[3]='e';
-		country[4]='a';
-		
-//		double country[]=new double[1];
-//		country[0]=94.5;
+		double score[];
+		score=new double[1];
+		score[0]=94.5;
+
+//		double score[]=new double[1];
+//		score[0]=94.5;
 //		
-//		double country[]= {94.5};
-//		System.out.println(country[0]);
+//		double score[]= {94.5};
+//		System.out.println(score[0]);
 	}
 }
