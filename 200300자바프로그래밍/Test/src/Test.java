0197d23898859ea3a0aefbe9c418bfb3d97405d0
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-30 00:41:19 +0900

    배열_calculation

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,20 +1,21 @@
 /*
-"Korea", "UK", "UK"를 배열 grade에 저장하는 코드를 작성하시오
-3가지 서로 다른 방법으로 작성해 보시오
+변수 x, y에 저장된 두 정수의 합, 차, 곱, 몫(정수)을 배열 stat에
+저장한 후 반복문을 사용하여 stat의 내용을 출력하는 코드를 작
+성하시오
+변수 x, y는 프로그램 내에 그 값을 입력해 두시오
+x=10, y=3에 대해 테스트하시오
 */
 public class Test {
 	public static void main(String[] args) {
-		String grade[];
-		grade = new String[3];
-		grade[0] = "Korea";
-		grade[1] = "UK";
-		grade[2] = "UK";
+		int x = 10, y = 3;
+		int stat[] = new int[4];
+		stat[0] = x + y;
+		stat[1] = x - y;
+		stat[2] = x * y;
+		stat[3] = x / y;
 
-//		String grade[] = new String[3];
-//		grade[0] = "Korea";
-//		grade[1] = "UK";
-//		grade[2] = "UK";
-//
-//		String grade[] = { "Korea", "UK", "UK" };
+		for (int i = 0; i < stat.length; i++) {
+			System.out.println(stat[i]);
+		}
 	}
 }
