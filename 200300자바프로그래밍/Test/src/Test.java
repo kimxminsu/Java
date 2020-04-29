07acf99fad9349d4de457f2f86344db33722baa7
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-04-29 21:23:37 +0900

    배열

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,29 +1,13 @@
-import javax.swing.JOptionPane;
-
 public class Test {
 	public static void main(String[] args) {
-		while(true) {
-			String scoreStr = JOptionPane.showInputDialog("성적(0-100)을 입력하세요");
-			double score = Integer.parseInt(scoreStr);
-			if(scoreStr==null) break;
-			char grade = ' ';
-			if (score >= 90) {
-				grade = 'A';
-			}
-			if (score >= 80 && score < 90) {
-				grade = 'B';
-			}
-			if (score >= 70 && score < 80) {
-				grade = 'C';
-			}
-			if (score >= 60 && score < 70) {
-				grade = 'D';
-			}
-			if (score < 70)
-				grade = 'F';
-			String message = "성적 " + score + " 의 학점은 " + grade + "입니다.";
-			JOptionPane.showMessageDialog(null, message);
-			}
+		double score[];
+		score=new double[1];
+		score[0]=94.5;
 		
+//		double score[]=new double[1];
+//		score[0]=94.5;
+//		
+//		double score[]= {94.5};
+//		System.out.println(score[0]);
 	}
 }
