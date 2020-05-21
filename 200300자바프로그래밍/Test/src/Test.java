9ce6db018cfdb93c6553e2b1649862950fa5804c
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-21 15:22:33 +0900

    메소드_printSum()

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,18 +1,17 @@
 /*
-아래 코드의 func()는 세 정수 x, y, z를 파라미터로 전달받아
-(x-y)/z의 값을 double 자료형 실수로 반환하는 메소드이다. func()
-메소드를 정의하여 아래 코드를 완성하시오.
+아래 printSum()은 두 정수를 파라미터로 전달받아 그 합을 출력
+하는 메소드이다. 아래 코드가 정상 동작하도록 printSum() 메소
+드를 완성하시오.
 */
 public class Test {
 	public static void main(String[] args) {
-		int x = 1, y = 2, z = 2;
-		double value = func(x, y, z);
-		System.out.println(value); // 실행결과 -0.5
+		int x = 10, y = 30;
+		printSum(x, y); // 실행결과 40
 	}
 
-	private static double func(int x, int y, int z) {
-		double result=0;
-		result=(double)(x-y)/(double)z;
-		return result;
+	private static void printSum(int x, int y) {
+		int sum = 0;
+		sum = x + y;
+		System.out.println(sum);
 	}
 }
