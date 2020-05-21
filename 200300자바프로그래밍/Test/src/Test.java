8616a92bc1d9d7deabe883331ec4dff442a79518
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-21 15:17:45 +0900

    메소드_func()

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,21 +1,18 @@
 /*
-클래스 Test는 아래 실행 예시와 같이 명령행 인자로 주어진 두
-숫자 문자열에 대응하는 수의 합을 출력한다고 한다. 클래스
-Test를 작성하시오.
-
-java Test 11 22
-33
-명령프롬프트에서의 실행 예시
+아래 코드의 func()는 세 정수 x, y, z를 파라미터로 전달받아
+(x-y)/z의 값을 double 자료형 실수로 반환하는 메소드이다. func()
+메소드를 정의하여 아래 코드를 완성하시오.
 */
 public class Test {
 	public static void main(String[] args) {
-		int num1 = Integer.parseInt(args[0]);
-		int num2 = Integer.parseInt(args[1]);
-		System.out.println(num1+num2);
+		int x = 1, y = 2, z = 2;
+		double value = func(x, y, z);
+		System.out.println(value); // 실행결과 -0.5
+	}
+
+	private static double func(int x, int y, int z) {
+		double result=0;
+		result=(double)(x-y)/(double)z;
+		return result;
 	}
 }
-/*
-실행방법
-방법1)이클립스 상단-실행버튼 자세히-Run Configuration-java Application-Test-arguments-Program arguments-11 22 입력
-방법2)cmd-cd 파일경로(src)-javac Test.java-(Test.class 생성됨)-java Test 11 22
-*/
