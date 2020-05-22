ca095eb01f7778e3ee92af6d449df5361aba115b
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-22 12:34:59 +0900

    문자열_실습B_substring()

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,18 +1,18 @@
 /*
-문자열 실습 A
-다음은 입력창에 길이 1 이상의 문자열을 입력 후 OK버튼을 클릭하면 입력된 문자
-열의 첫 문자와 마지막 문자를 화면에 출력하는 코드와 그 실행 예시이다. 아래 코드
-를 완성하시오.
+문자열 실습 B
+다음은 입력창에 길이 3이상의 문자열을 입력 후 OK버튼을 클릭하면 입력된 문자열
+의 왼쪽 끝 세 문자들로 이루어진 문자열과 오른쪽 끝 세 문자들로 이루어진 문자열
+을 화면에 출력하는 코드와 그 실행 예시이다. 아래 코드를 완성하시오.
 */
 import javax.swing.JOptionPane;
 
 public class Test {
 	public static void main(String[] args) {
 		String s = JOptionPane.showInputDialog("문자열 입력");
-		char firstChar = s.charAt(0);
-		char lastChar = s.charAt(s.length()-1);
-		System.out.println(firstChar);
-		System.out.println(lastChar);
+		String prefix = s.substring(0, 3);
+		String postfix = s.substring(s.length() - 3);
+		System.out.println(prefix);
+		System.out.println(postfix);
 	}
 }
 /*
@@ -20,6 +20,6 @@ public class Test {
 경성대학교 소프트웨어학과
 
 실행결과
-경
-과
+경성대
+어학과
 */
