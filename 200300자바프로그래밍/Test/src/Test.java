cd34634916cf3d4b2c0d4e6b39d0de0db40b6cbf
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-22 18:54:10 +0900

    문자열_실습E_split()

--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,25 +1,41 @@
 /*
-문자열 실습 B
-다음은 입력창에 길이 3이상의 문자열을 입력 후 OK버튼을 클릭하면 입력된 문자열
-의 왼쪽 끝 세 문자들로 이루어진 문자열과 오른쪽 끝 세 문자들로 이루어진 문자열
-을 화면에 출력하는 코드와 그 실행 예시이다. 아래 코드를 완성하시오.
+문자열 실습 E
+다음은 입력창에 대쉬문자(-)로 분리된 2개 이상의 국가명을 입력 후 OK버튼을 클릭
+하면 입력된 국가명 중 가장 왼쪽 국가명과 가장 오른쪽 국가명을 화면에 출력하는
+코드와 그 실행 예시이다. 아래 코드를 완성하시오.
 */
 import javax.swing.JOptionPane;
 
 public class Test {
 	public static void main(String[] args) {
 		String s = JOptionPane.showInputDialog("문자열 입력");
-		String prefix = s.substring(0, 3);
-		String postfix = s.substring(s.length() - 3);
-		System.out.println(prefix);
-		System.out.println(postfix);
+		String t[]=s.split("-");
+		System.out.println(t[0]);
+		System.out.println(t[t.length-1]);
 	}
 }
 /*
 입력값
-경성대학교 소프트웨어학과
+한국-미국-영국-독일-스위스
 
 실행결과
-경성대
-어학과
+한국
+스위스
+*/
+
+/*
+L14
+문제점:
+t[t.length]라고 생각해서 계속 오류가 일어났다.
+혹시나 문자의 길이를 구할때처럼 t.length()를 시도했으나
+	System.out.println(t[t.length()]);
+또 오류가 났고,
+[]안에는 t.length가 안되는가 싶어서 새로운 변수에 t.length를 넣어서 시도했으나
+	int n = t.length;
+	System.out.println(t[n]);
+여전히 오류가 발생했다.
+
+해결:
+t.length는 5이기 때문에 t[5]는 존재하지 않는다.
+따라서 t[t.length-1]로 수정해야한다.
 */
