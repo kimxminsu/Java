1962d31bc5cabea62e158d48448f858361f58045
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-25 22:21:04 +0900

    클래스_실습A_Location() 기본생성자, country,city대입생성자

---
+++ 200300자바프로그래밍/Test/src/Location.java
@@ -0,0 +1,13 @@
+
+public class Location {
+	String country, city;
+
+	public Location() {
+
+	}
+
+	public Location(String country, String city) {
+		this.country = country;
+		this.city = city;
+	}
+}
--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,29 +1,20 @@
 /*
-객체실습 F
-점수 클래스 Score를 다음과 같이 정의하시오
-필드변수명(설명,자료형): langScore(국어점수,double), mathScore(수학점수,double), engScore(영어점수,double)
-학생 클래스 Student를 다음과 같이 정의하시오
-필드변수명(설명,자료형): id(학번,String), score(점수,Score)
-다음은 클래스 Student의 객체를 생성한 후 그 정보를 출력하는 코드와 그 실행 결과를 보인 것이다. 아래 코드의 빈 곳을 완성하시오
+클래스 실습 A
+다음 조건에 따라 클래스 Location을 정의하시오
+필드변수명(설명,자료형): country(국가명,String), city(도시명,String)
+국가명과 도시명을 파라미터로 전달받아 대응하는 필드에 대입하는 생성자
+다음은 Location 객체를 생성하고 그 내용을 출력하는 코드와 실행 결과를 보인 것이다. 이 코드가 정
+상 동작하도록 Location 클래스를 정의하시오.
 */
 public class Test {
 	public static void main(String[] args) {
-		Student s;
-		s=new Student();
-		s.id="S-001";
-		s.score=new Score();
-		s.score.langScore=100.0;
-		s.score.engScore=80.0;
-		s.score.mathScore=90.0;
-		System.out.println("학번: " + s.id);
-		System.out.println("국어점수: " + s.score.langScore);
-		System.out.println("영어점수: " + s.score.engScore);
-		System.out.println("수학점수: " + s.score.mathScore);
+		Location loc;
+		loc = new Location("한국", "부산");
+		System.out.println("국가:" + loc.country);
+		System.out.println("도시:" + loc.city);
 	}
 }
 /*
-학번: S-001
-국어점수: 100.0
-영어점수: 80.0
-수학점수: 90.0
+국가:한국
+도시:부산
 */
