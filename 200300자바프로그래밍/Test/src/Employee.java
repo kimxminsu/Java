102714b540c4114d28aa6321466eba9fb69b76df
kimxminsu <35947676+kimxminsu@users.noreply.github.com>
2020-05-29 17:27:39 +0900

    오버로딩_실습A_Employee(), Employee( , )

--- 200300자바프로그래밍/Test/src/Employee.java
+++ 200300자바프로그래밍/Test/src/Employee.java
@@ -1,5 +1,17 @@
-
 public class Employee {
 	String id;
-	DateInfo joinDate;
+	double monthlyPayBase = 250; // 기본 월 급여액 250만원
+
+	public Employee(String id) {
+		this.id = id;
+	}
+
+	public Employee(String id, int monthlyPayBase) {
+		this.id = id;
+		this.monthlyPayBase = monthlyPayBase;
+	}
+
+	public double getMonthlyPay() {
+		return monthlyPayBase;
+	}
 }
--- 200300자바프로그래밍/Test/src/Test.java
+++ 200300자바프로그래밍/Test/src/Test.java
@@ -1,28 +1,14 @@
 /*
-private 실습 A
+오버로딩 실습 A
 회원 클래스 Member를 다음 조건에 따라 작성하시오
-필드변수명(설명,자료형): email(메일주소,String), joinDate(가입일-8자리,String)
-클래스 외부로부터의 직접 접근 불허 설정할 것
-메일주소와 가입일을 파라미터로 전달받아 대응하는 필드에 대입하는 생성자
-회원의 email을 반환하는 public 메소드 getEmail()
-새로운 메일주소를 파라미터로 전달받아 객체의 기존 메일주소를 변경하는 메소드 setEmail()
-회원의 joinDate를 8자리 숫자 문자열로 반환하는 public 메소드 getJoinDate()
-회원의 가입년도를 4자리 숫자 문자열로 반환하는 public 메소드 getJoinYear()
-
-클래스 Test의 main() 내 다음 절차를 코딩하시오
-Member 객체(메일주소 yhkim@ks.ac.kr, 가입일 20190214 ) 생성
-Member 객체의 메일주소와 가입일을 출력
-Member 객체의 메일주소를 yhkim@cs.ks.ac.kr로 변경
-Member 객체의 메일주소와 가입일을 출력
-Member 객체의 가입년도를 출력
+	아래 Test 클래스가 정상 동작하도록 Employee 클래스를 수정하시오
+	생성자 오버로딩 시 this()를 사용하시오
 */
 public class Test {
 	public static void main(String[] args) {
-		Member member;
-		member=new Member("yhkim@ks.ac.kr", "20190214");
-		System.out.println(member);
-		member.setEmail("yhkim@cs.ks.ac.kr");
-		System.out.println(member);
-		System.out.println(member.getJoinYear());
+		Employee e1 = new Employee("E-001"); // 일반 직원
+		Employee e2 = new Employee("E-002", 100); // 인턴 직원, 기본 월 급여액 100만원
+		System.out.println(e1.getMonthlyPay()); // 월 급여액 출력
+		System.out.println(e2.getMonthlyPay()); // 월 급여액 출력
 	}
 }
